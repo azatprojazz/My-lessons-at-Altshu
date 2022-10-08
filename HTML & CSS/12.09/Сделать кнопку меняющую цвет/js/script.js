6725483bbdc0d7e734622e741896290d7e002443
azatprojazz <azatprojazz@gmail.com>
2022-10-08 15:07:22 +0300

    Обнова

--- HTML & CSS/12.09/Сделать кнопку меняющую цвет/js/script.js
+++ HTML & CSS/12.09/Сделать кнопку меняющую цвет/js/script.js
@@ -3,9 +3,9 @@ const button = document.getElementById('btn');
 const colors = ['red', 'yellow', 'blue', 'white', 'green', 'black'];
 
 button.addEventListener('click', () => {
-  let hexColor = colors[getRandomNumber()];
-  document.querySelector('.color').style.backgroundColor = hexColor;
-  colors.textContent = hexColor;
+  let Color = colors[getRandomNumber()];
+  document.querySelector('.color').style.backgroundColor = Color;
+  colors.textContent = Color;
 });
 
 function getRandomNumber() {
