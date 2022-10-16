670564230e40a872a78223e82f5670487cd4aadc
azatprojazz <azatprojazz@gmail.com>
2022-10-16 20:10:26 +0300

    Докуртил к кнопкам светлую тень, при смене темы

--- HTML & CSS/26.09/Итоговый проект/js/script.js
+++ HTML & CSS/26.09/Итоговый проект/js/script.js
@@ -2,14 +2,32 @@ const body = document.querySelector('body');
 const lightThemeBtn = document.querySelector('.light-theme-btn');
 const darkThemeBtn = document.querySelector('.dark-theme-btn');
 const randomThemeBtn = document.querySelector('.random-theme-btn');
+let buttons = document.querySelectorAll('button');
 
 function turnOnLightTheme() {
   body.style.color = '#373737';
   body.style.background = 'white';
+  buttons.forEach(function (button) {
+    button.addEventListener('mouseover', function () {
+      button.style.boxShadow = '0 0 7px 0px black';
+    });
+    button.addEventListener('mouseout', function () {
+      button.style.boxShadow = 'none';
+    });
+  });
 }
+
 function turnOnDarktTheme() {
   body.style.color = 'white';
   body.style.background = '#373737';
+  buttons.forEach(function (button) {
+    button.addEventListener('mouseover', function () {
+      button.style.boxShadow = '0 0 7px 0px white';
+    });
+    button.addEventListener('mouseout', function () {
+      button.style.boxShadow = 'none';
+    });
+  });
 }
 
 function getRandomNum(min, max) {
@@ -18,7 +36,10 @@ function getRandomNum(min, max) {
 }
 
 function generateRandomRGAColor() {
-  let randomColor = `rgb(${getRandomNum(0, 255)}, ${getRandomNum(0, 255)}, ${getRandomNum(0, 255)})`;
+  let randomColor = `rgb(${getRandomNum(0, 255)}, ${getRandomNum(
+    0,
+    255
+  )}, ${getRandomNum(0, 255)})`;
   return randomColor;
 }
 
