882592441c9d384f185be3159d70b44a19771481
azatprojazz <azatprojazz@gmail.com>
2022-11-07 13:56:25 +0300

    Правки

--- Javascript/04.11.22/accumulator.js
+++ Javascript/04.11.22/accumulator.js
@@ -3,7 +3,7 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let accumulatorOdd = arr;
 
-for (i = 0; i < accumulatorOdd.length; i += 2) {
+for (let i = 0; i < accumulatorOdd.length; i += 2) {
   console.log(accumulatorOdd[i]);
 } */
 
@@ -11,7 +11,7 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let accumulatorOdd = arr;
 
-for (i = 0; i < accumulatorOdd.length; i++) {
+for (let i = 0; i < accumulatorOdd.length; i++) {
   if (accumulatorOdd[i] % 2 == 1) {
     console.log(accumulatorOdd[i]);
   }
--- Javascript/04.11.22/arrays-3.js
+++ Javascript/04.11.22/arrays-3.js
@@ -1,6 +1,6 @@
 let arr = [55, 43, 23, 19, -5, -8, -15, 14, 9, 11];
 
-for (i = 0; i < arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
   if (arr[i] > 0) {
     console.log(arr[i]);
   }
