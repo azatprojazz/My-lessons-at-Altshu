14e5ee8ba30c5e7b10be1d915e16c91dcf366d90
azatprojazz <azatprojazz@gmail.com>
2022-11-07 13:51:02 +0300

    Правки

--- Javascript/04.11.22/accumulator.js
+++ Javascript/04.11.22/accumulator.js
@@ -1,20 +1,18 @@
-// Альтернативный вариант решения
-
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// let accumulatorOdd = arr;
-
-// for (i = 0; i < accumulatorOdd.length; i++) {
-//   if (accumulatorOdd[i] % 2 == 1) {
-//     console.log(accumulatorOdd[i]);
-//   }
-// }
-
-
+/* Альтернативный вариант решения
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let accumulatorOdd = arr;
 
 for (i = 0; i < accumulatorOdd.length; i += 2) {
   console.log(accumulatorOdd[i]);
+} */
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let accumulatorOdd = arr;
+
+for (i = 0; i < accumulatorOdd.length; i++) {
+  if (accumulatorOdd[i] % 2 == 1) {
+    console.log(accumulatorOdd[i]);
+  }
 }
