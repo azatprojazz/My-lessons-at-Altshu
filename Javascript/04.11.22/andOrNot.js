8404ab6d65cb719c4c60ee92296e11a2f6b93df3
azatprojazz <azatprojazz@gmail.com>
2022-11-07 23:32:50 +0300

    Правки

--- Javascript/04.11.22/andOrNot.js
+++ Javascript/04.11.22/andOrNot.js
@@ -1,10 +1,10 @@
-let dayToday = prompt('Какой сегодня день недели по счету (от 1 до 7)', '');
-let dayHour = prompt('Который час без минут (от 0 до 23)', '');
-let dayMinut = prompt('Сколько минут (от 0 до 59)', '');
+let dayToday = +prompt('Какой сегодня день недели по счету (от 1 до 7)', '');
+let dayHour = +prompt('Который час без минут (от 0 до 23)', '');
+let dayMinut = +prompt('Сколько минут (от 0 до 59)', '');
 
 if (dayToday % 2 !== 0 && dayToday !== 7 && dayHour < 19) {
   alert('Да, сегодня планируется занятие');
-} else if (dayToday == 2 || dayToday == 4 || dayToday == 7) {
+} else if (dayToday === 2 || dayToday === 4 || dayToday === 7) {
   alert('Завтра будет занятие');
 } else if (
   dayToday % 2 !== 0 &&
