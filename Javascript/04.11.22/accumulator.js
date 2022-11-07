bcfd14831d2fbf42c64a6992890e0d331428498a
azatprojazz <azatprojazz@gmail.com>
2022-11-08 02:32:17 +0300

    Исправления

--- Javascript/04.11.22/accumulator.js
+++ Javascript/04.11.22/accumulator.js
@@ -1,12 +1,3 @@
-/* Альтернативный вариант решения
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-let accumulatorOdd = arr;
-
-for (let i = 0; i < accumulatorOdd.length; i += 2) {
-  console.log(accumulatorOdd[i]);
-} */
-
 let arr = [1, 223, -21, 44, 51, 62, -7, 48,-19, 10];
 let accumulatorOdd = [];
 
